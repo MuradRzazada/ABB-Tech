da2ce842fc552c889e29ea4f594a2fa76e8f8753
MuradRzazada <160116606+MuradRzazada@users.noreply.github.com>
2024-03-03 16:01:41 +0400

    File Uploaded

--- abbproject/src/main/java/org/abbtech/lesson10/Task2/Main.java
+++ abbproject/src/main/java/org/abbtech/lesson10/Task2/Main.java
@@ -2,9 +2,15 @@ package org.abbtech.lesson10.Task2;
 
 import java.util.Scanner;
 
-import static org.abbtech.lesson10.Task2.CustomException.checkInput;
-
 public class Main {
+    public static void checkInput(int input) throws InvalidInputException {
+        int min = 0;
+        int max = 100;
+
+        if (input < min || input > max) {
+            throw new InvalidInputException("Input age should be between " + min + " and " + max);
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter age between 0 and 100:");
@@ -13,8 +19,8 @@ public class Main {
             checkInput(user);
             System.out.println("Input accepted: " + user);
         } catch (InvalidInputException invalidInputException){
-        } finally {
-            scanner.close();
+            System.out.println("Invalid input: " + invalidInputException.getMessage());
+
         }
     }
 }
